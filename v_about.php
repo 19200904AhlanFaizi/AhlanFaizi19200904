0eb70b3b4fe533276eaa693ba30a0ce7ab720600
19200904AhlanFaizi <91113837+19200904AhlanFaizi@users.noreply.github.com>
2021-10-11 13:29:15 +0700

    Tugas Pertemuan 4

---
+++ v_about.php
@@ -0,0 +1,21 @@
+<section> 
+    <h1><?php echo $judul ?></h1>
+    <h4>Nama</h4> 
+    <ul type="disc"> 
+        <li>Nama Depan : Ahlan</li> 
+        <li>Nama Belakang : Faizi</li> 
+    </ul> 
+    <br> 
+    <h4>Alamat</h4> 
+    <ul type="disc"> 
+        <li> Jl.Mampang Prapatan XI</li> 
+    </ul> 
+    <h4>Tempat Lahir</h4> 
+    <ul type="disc"> 
+        <li>Jakarta</li> </ul> 
+    <h4>Olah Raga Favorit</h4> 
+    <ul type="disc"> 
+        <li>sepak Bola</li> 
+        <li>Golf</li> 
+    </ul> 
+</section> 
